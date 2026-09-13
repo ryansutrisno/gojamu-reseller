0b3cd9e840ade6bd503d1efca132e5c20d4378df
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2026-09-13 18:58:16 +0700

    fix: trust proxies to fix HTTPS URL generation behind Dokploy proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);

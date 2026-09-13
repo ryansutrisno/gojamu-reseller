ad5823639d19742b1421340f0454c68eaed0cf5e
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2026-09-13 19:09:48 +0700

    fix: use relative redirect for logout to ensure redirect to login works behind proxy

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -60,6 +60,6 @@ class AuthenticatedSessionController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect()->route('login');
+        return redirect(route('login', absolute: false));
     }
 }
